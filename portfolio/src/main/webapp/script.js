7cf38f743ab7244bc1606147df23da737205b669
Jaya Gupta <jayagupta_01@hotmail.com>
2020-06-20 14:40:44 -0400

    Add custom markers to map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,14 +14,14 @@
 
 
 async function getUrlData() {
-  const response = await fetch('/data');
-  const urlData = await response.json();
-//   const urlData = await response.text();
-  console.log(urlData);
-  document.getElementById('data-container').innerText = urlData;
-}
-
-function createMap() {
+    const response = await fetch('/data');
+    const urlData = await response.json();
+  //   const urlData = await response.text();
+    console.log(urlData);
+    document.getElementById('data-container').innerText = urlData;
+  }
+  
+  function initMap() {
     const map = new google.maps.Map(document.getElementById('map'),{
         center: {lat: 30, lng: 0}, 
         zoom: 1, 
@@ -195,4 +195,31 @@ function createMap() {
             }
           ]
     });
-  }
+    var newyorkcity = {lat: 40.7128, lng: -74.0060};
+    var orlando = {lat: 28.5383, lng: -81.3792};
+    var mumbai = {lat: 19.0760, lng: 72.8777};
+    var dc = {lat: 38.9072, lng: -77.0369};
+
+    var banff = {lat: 51.4968, lng:-115.9281};
+    var losangeles = {lat: 34.0522, lng:-118.2437};
+    var paris = {lat: 48.8566, lng: 2.3522};
+    var morocco = {lat: 31.7917, lng: -7.0926};
+    var switzerland = {lat: 46.8182, lng:8.2275};
+    var australia = {lat: -25.2744, lng:133.7751};
+    var egypt = {lat: 26.8206, lng:30.8025};
+
+    //Places I have visited
+    var marker = new google.maps.Marker({position: newyorkcity, icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}, map: map, title: 'New York City - my favourite city'});
+    var marker = new google.maps.Marker({position: orlando, icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}, map: map, title: 'Orlando, FL, USA'});
+    var marker = new google.maps.Marker({position: mumbai, icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}, map: map, title: 'Mumbai, India'});
+    var marker = new google.maps.Marker({position: dc, icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}, map: map, title: 'Washington, DC, USA'});
+
+    //Places I want to visit
+    var marker = new google.maps.Marker({position: banff, icon: {url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}, map: map, title: 'Banff National Park, Alberta, Canada'});
+    var marker = new google.maps.Marker({position: losangeles, icon: {url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}, map: map, title: 'Los Angeles, CA, USA'});
+    var marker = new google.maps.Marker({position: paris, icon: {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}, map: map, title: 'Paris, France'});
+    var marker = new google.maps.Marker({position: morocco, icon: {url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}, map: map, title: 'Morocco'});
+    var marker = new google.maps.Marker({position: switzerland, icon: {url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}, map: map, title: 'Switzerland'});
+    var marker = new google.maps.Marker({position: australia, icon: {url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}, map: map, title: 'Australia'});
+    var marker = new google.maps.Marker({position: egypt, icon: {url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}, map: map, title: 'Egypt'});
+}
